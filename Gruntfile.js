e53a2450d744edb9b51b143d890ea2ba1f7c4d25
xthursdayx <xthursdayx@mailbox.org>
2022-06-28 23:29:00 -0400

    Update to head

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
+const path = require('path')
+
 module.exports = function (grunt) {
-
   grunt.initConfig({
-
     watch: {
       js: {
         files: ['src/**/*.js'],
@@ -16,39 +16,39 @@ module.exports = function (grunt) {
         options: {
           spawn: false,
         },
-      }
+      },
     },
 
     sass: {
       dist: {
         options: {
           style: 'expanded',
-          sourceMap: false
+          sourceMap: false,
         },
         files: {
-          'dist/app.css': 'src/main.scss'
-        }
-      }
+          'dist/app.css': 'src/main.scss',
+        },
+      },
     },
 
     babel: {
       options: {
         sourceMap: true,
-        presets: ['@babel/preset-env']
+        presets: ['@babel/preset-env'],
       },
       app: {
         files: {
-          'dist/app.js': ['dist/app.js']
-        }
-      }
+          'dist/app.js': ['dist/app.js'],
+        },
+      },
     },
 
     browserify: {
       dist: {
         files: {
-          'dist/app.js': 'dist/app.js'
-        }
-      }
+          'dist/app.js': 'dist/app.js',
+        },
+      },
     },
 
     concat: {
@@ -57,20 +57,18 @@ module.exports = function (grunt) {
       },
 
       app: {
-        src: [
-          'src/**/*.js',
-        ],
+        src: ['src/**/*.js'],
         dest: 'dist/app.js',
       },
 
       lib: {
         src: [
-          'node_modules/codemirror/lib/codemirror.js',
-          'node_modules/@standardnotes/component-relay/dist/dist.js',
+          require.resolve('codemirror/lib/codemirror.js'),
+          require.resolve('@standardnotes/component-relay/dist/dist.js'),
           'vendor/modes/markdown/markdown.js',
           'vendor/addon/modes/overlay.js',
           'vendor/modes/gfm/gfm.js',
-          'vendor/mark-selection.js'
+          'vendor/mark-selection.js',
         ],
         dest: 'dist/lib.js',
       },
@@ -84,26 +82,44 @@ module.exports = function (grunt) {
         options: {
           separator: '',
         },
-        src: ['node_modules/codemirror/lib/codemirror.css', 'node_modules/sn-stylekit/dist/stylekit.css', 'dist/app.css'],
+        src: [
+          require.resolve('codemirror/lib/codemirror.css'),
+          require.resolve('sn-stylekit/dist/stylekit.css'),
+          'dist/app.css',
+        ],
         dest: 'dist/dist.css',
-      }
+      },
     },
     copy: {
       main: {
         files: [
-          { expand: true, cwd: 'node_modules/', src: ['sn-codemirror-search/**'], dest: 'dist/' },
+          {
+            expand: true,
+            cwd: path.dirname(require.resolve('sn-codemirror-search')),
+            src: ['**', '!package.json'],
+            dest: 'dist/sn-codemirror-search',
+          },
         ],
       },
     },
-  });
+  })
 
-  grunt.loadNpmTasks('grunt-newer');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-babel');
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-newer')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-babel')
+  grunt.loadNpmTasks('grunt-browserify')
+  grunt.loadNpmTasks('grunt-contrib-concat')
+  grunt.loadNpmTasks('grunt-contrib-sass')
+  grunt.loadNpmTasks('grunt-contrib-copy')
 
-  grunt.registerTask('default', ['concat:app', 'babel', 'browserify', 'concat:lib', 'concat:dist', 'sass', 'concat:css', 'copy']);
-};
+  grunt.registerTask('default', [
+    'concat:app',
+    'babel',
+    'browserify',
+    'concat:lib',
+    'concat:dist',
+    'sass',
+    'concat:css',
+    'copy',
+  ])
+}
--- src/main.js
+++ src/main.js
@@ -1,33 +1,32 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-
-  let componentRelay;
-  let workingNote, clientData;
-  let lastValue, lastUUID;
-  let editor;
-  let ignoreTextChange = false;
-  let initialLoad = true;
+document.addEventListener('DOMContentLoaded', function (event) {
+  let componentRelay
+  let workingNote, clientData
+  let lastValue, lastUUID
+  let editor
+  let ignoreTextChange = false
+  let initialLoad = true
 
   function loadComponentRelay() {
-    const initialPermissions = [{ name: "stream-context-item" }]
+    const initialPermissions = [{ name: 'stream-context-item' }]
     componentRelay = new ComponentRelay({
       initialPermissions,
       targetWindow: window,
       onReady: function () {
-        const platform = componentRelay.platform;
+        const platform = componentRelay.platform
         if (platform) {
-          document.body.classList.add(platform);
+          document.body.classList.add(platform)
         }
 
-        loadEditor();
+        loadEditor()
 
         // only use CodeMirror selection color if we're not on mobile.
-        editor.setOption("styleSelectedText", !componentRelay.isMobile);
-      }
-    });
+        editor.setOption('styleSelectedText', !componentRelay.isMobile)
+      },
+    })
 
     componentRelay.streamContextItem((note) => {
-      onReceivedNote(note);
-    });
+      onReceivedNote(note)
+    })
   }
 
   function saveNote() {
@@ -35,75 +34,74 @@ document.addEventListener("DOMContentLoaded", function (event) {
       // Be sure to capture this object as a variable, as this.note may be reassigned in `streamContextItem`, so by the time
       // you modify it in the presave block, it may not be the same object anymore, so the presave values will not be applied to
       // the right object, and it will save incorrectly.
-      let note = workingNote;
+      let note = workingNote
 
       componentRelay.saveItemWithPresave(note, () => {
-        lastValue = editor.getValue();
-        note.content.text = lastValue;
-        note.clientData = clientData;
+        lastValue = editor.getValue()
+        note.content.text = lastValue
+        note.clientData = clientData
 
         // clear previews
-        note.content.preview_plain = null;
-        note.content.preview_html = null;
-      });
+        note.content.preview_plain = null
+        note.content.preview_html = null
+      })
     }
   }
 
   function onReceivedNote(note) {
     if (note.uuid !== lastUUID) {
       // Note changed, reset last values
-      lastValue = null;
-      initialLoad = true;
-      lastUUID = note.uuid;
+      lastValue = null
+      initialLoad = true
+      lastUUID = note.uuid
     }
 
-    workingNote = note;
+    workingNote = note
 
     // Only update UI on non-metadata updates.
     if (note.isMetadataUpdate) {
-      return;
+      return
     }
 
-    clientData = note.clientData;
+    clientData = note.clientData
 
     if (editor) {
       if (note.content.text !== lastValue) {
-        ignoreTextChange = true;
-        editor.getDoc().setValue(workingNote.content.text);
-        ignoreTextChange = false;
+        ignoreTextChange = true
+        editor.getDoc().setValue(workingNote.content.text)
+        ignoreTextChange = false
       }
 
       if (initialLoad) {
-        initialLoad = false;
-        editor.getDoc().clearHistory();
+        initialLoad = false
+        editor.getDoc().clearHistory()
       }
 
-      editor.setOption(
-        "spellcheck",
-        workingNote.content.spellcheck
-      );
+      editor.setOption('spellcheck', workingNote.content.spellcheck)
     }
   }
 
   function loadEditor() {
-    editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-      mode: "gfm",
+    editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+      mode: 'gfm',
       lineWrapping: true,
-      extraKeys: { "Alt-F": "findPersistent" },
-      inputStyle: getInputStyleForEnvironment()
-    });
-    editor.setSize(undefined, "100%");
+      extraKeys: { 'Alt-F': 'findPersistent' },
+      inputStyle: getInputStyleForEnvironment(),
+    })
+    editor.setSize(undefined, '100%')
 
-    editor.on("change", function () {
-      if (ignoreTextChange) { return; }
-      saveNote();
-    });
+    editor.on('change', function () {
+      if (ignoreTextChange) {
+        return
+      }
+      saveNote()
+    })
   }
 
   function getInputStyleForEnvironment() {
-    const environment = componentRelay.environment ?? 'web';
-    return environment === 'mobile' ? 'textarea' : 'contenteditable';
+    const environment = componentRelay.environment ?? 'web'
+    return environment === 'mobile' ? 'textarea' : 'contenteditable'
   }
 
-  loadComponentRelay();
-});
+  loadComponentRelay()
+})
